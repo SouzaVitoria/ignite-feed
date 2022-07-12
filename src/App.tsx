8a93b2b85edd18675f80ce2c42fbd0ccfa4a568f
Vitória Souza <vitoria.souza+github@acct.global>
2022-07-12 09:45:21 -0300

    added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header/Header"
 import "./global.css"
 
 export function App() {
   return (
     <div>
+      <Header />
       <h2>Ignite Feed</h2>
     </div>
   )
